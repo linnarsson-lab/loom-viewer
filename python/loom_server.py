5e9956b6095d5e7b9e0fb70871221ec55386ae97
Sten Linnarsson <sten.linnarsson@ki.se>
2016-01-16 14:58:53 +0100

    Fixed cache expiry bug

--- python/loom_server.py
+++ python/loom_server.py
@@ -9,9 +9,6 @@ from flask import make_response
 from functools import wraps, update_wrapper
 from datetime import datetime
 
-os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-print "Serving from: " + os.getcwd()
-
 if len(sys.argv) != 2:
 	print "ERROR: a single argument is required (name of h5 file to browse)"
 	sys.exit(1)
@@ -23,8 +20,8 @@ except:
 	sys.exit(1)
 
 # Precompute zoom pyramid and tSNE
-#if not ds.loom_is_prepared():
-ds.loom_prepare()
+if not ds.loom_is_prepared():
+	ds.loom_prepare()
 
 # Create fileinfo (javascript format)
 dims = ds.dz_dimensions()
@@ -39,56 +36,45 @@ fileinfo = "window.fileinfo = " + json.dumps({
 		"colAttrs": dict([(name, vals.tolist()) for (name,vals) in ds.col_attrs.iteritems()])
 	})
 
-def nocache(view):
-	@wraps(view)
-	def no_cache(*args, **kwargs):
-		response = make_response(view(*args, **kwargs))
-		response.headers['Last-Modified'] = datetime.now()
-		response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-		response.headers['Pragma'] = 'no-cache'
-		response.headers['Expires'] = '-1'
-		return response
-		
-	return update_wrapper(no_cache, view)
+class LoomServer(flask.Flask):
+	# Disable cacheing 
+    def get_send_file_max_age(self, name):
+        return 0
 
-app = flask.Flask(__name__)
+
+os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+print "Serving from: " + os.getcwd()
+app = LoomServer(__name__)
 
 #
 # Static assets
 #
 
 @app.route('/js/<path:path>')
-@nocache
 def send_js(path):
 	return flask.send_from_directory('/js', path)
 
 @app.route('/css/<path:path>')
-@nocache
 def send_css(path):
 	return flask.send_from_directory('/css', path)
 
 @app.route('/img/<path:path>')
-@nocache
 def send_img(path):
 	return flask.send_from_directory('/img', path)
 
 @app.route('/')
-@nocache
 def send_indexjs():
 	return app.send_static_file('index.html')
 
 @app.route('/fileinfo.js')
-@nocache
 def send_fileinfo():
 	return flask.Response(fileinfo, mimetype="application/javascript")
 
 @app.route('/row/<int:row>')
-@nocache
 def send_row(row):
 	return flask.Response(json.dumps(ds.file['/matrix'][row,:].tolist()), mimetype="application/json")
 
 @app.route('/col/<int:col>')
-@nocache
 def send_col(col):
 	return flask.Response(json.dumps(ds.file['/matrix'][:,col].tolist()), mimetype="application/json")
 
