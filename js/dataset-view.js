fb97223c565f085a6b597a3d4b6f16cf2b63b064
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-04-15 16:22:41 +0200

    Refactoring + cleanup

--- js/dataset-view.js
+++ js/dataset-view.js
@@ -4,10 +4,10 @@ import Dropzone from 'react-dropzone';
 
 export class DatasetView extends Component {
 
-	render() {
+	render () {
 		var dispatch = this.props.dispatch;
 		var ds = this.props.dataState;
-	  	var vs = this.props.viewState;
+		var vs = this.props.viewState;
 
 		var panels = Object.keys(ds.projects).map((proj) => {
 			var datasets = ds.projects[proj].map((d) => {
@@ -15,42 +15,44 @@ export class DatasetView extends Component {
 				console.log(d);
 				console.log(ds.currentDataset);
 				return (
-					<div key={d.dataset} className={"list-group-item" + (isCurrent ? " list-group-item-info" : "")}>
-						<a onClick={(event)=>dispatch(fetchDataset(d.transcriptome + "__" + proj + "__" + d.dataset))}>{d.dataset}</a>
+					<div key={d.dataset} className={"list-group-item" + (isCurrent ? " list-group-item-info" : "") }>
+						<a onClick={(event) => dispatch(fetchDataset(d.transcriptome + "__" + proj + "__" + d.dataset)) }>{d.dataset}</a>
 						<span>{" " + d.message}</span>
 						<div className="pull-right">
 							<a>Delete</a> / <a>Duplicate</a> / <a>Edit</a>
 						</div>
-					</div>);
+					</div>
+				);
 			});
-			return <div key={proj} className="panel panel-primary">
-				<div className="panel-heading">
-					{proj}
-					<div className="pull-right">
-						<span>{ds.projects[proj].length.toString() + " dataset" + (ds.projects[proj].length > 1 ? "s" : "")}</span>
+			return (
+				<div key={proj} className="panel panel-primary">
+					<div className="panel-heading">
+						{proj}
+						<div className="pull-right">
+							<span>{ds.projects[proj].length.toString() + " dataset" + (ds.projects[proj].length > 1 ? "s" : "") }</span>
+						</div>
+					</div>
+					<div className="list-group">
+						{datasets}
 					</div>
 				</div>
-				<div className="list-group">
-				{datasets}
-				</div>
-			</div>;
+			);
 		});
+
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="view col-md-8">
-						<h3>&nbsp;</h3>
+						<h3>&nbsp; </h3>
 						<h3>Linnarsson lab single-cell data repository</h3>
-						<h3>&nbsp;</h3>
+						<h3>&nbsp; </h3>
+						<h4>Available datasets</h4>
 						<div>
-						{panels}
+							{panels.length === 0 ? "(loading...)" : panels}
 						</div>
+						<h4>Create a new dataset</h4>
 						<div>
-						{panels.length == 0 ? "(loading...)" : ""}
-						</div>
-						<div>
-						<FileUpload instructions="Click to select a CSV file with cell attributs (drag and drop also works)" />
-						<FileUpload instructions="(optional) Click to select a CSV file with cell attributs (drag and drop also works)" />
+							<CreateDataset />
 						</div>
 					</div>
 				</div>
@@ -65,12 +67,26 @@ DatasetView.propTypes = {
 	dispatch: PropTypes.func.isRequired
 }
 
+export class CreateDataset extends Component {
+	render (){
+		return (
+			<div class="panel panel-primary">
+				<div className="panel panel-heading">
+					<FileUpload instructions="Click to select a CSV file with cell attributs (drag and drop also works)" class="panel-body" />
+					<FileUpload instructions="(optional) Click to select a CSV file with cell attributs (drag and drop also works)" class="panel-body" />
+					<p>n_features: <input type="number" defaultValue="100" /></p>
+				</div>
+			</div>
+		);
+	}
+}
+
 export class FileUpload extends Component {
-	onDrop (file) {
-      console.log('Received file: ', file);
+	onDrop(file) {
+		console.log('Received file: ', file);
     }
 
-	render () {
+	render() {
 		return (
 			<Dropzone onDrop={this.onDrop} multiple={false}>
 				<div>{this.props.instructions}</div>
