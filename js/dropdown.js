99ef367917cac2c2e3f9c3328af6d2e7ea60aba0
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-05-17 12:37:17 +0200

    Created DropdownMenu component and added it to genescape-sidepanel.js

---
+++ js/dropdown.js
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react';
+
+export class DropdownMenu extends Component {
+
+	render() {
+
+		const {
+			buttonLabel, buttonName,
+			attributes, attrType, attrName,
+			dispatch,
+		} = this.props;
+
+		const options = attributes.map((name) => {
+			let dispatchParam = { type: attrType };
+			dispatchParam[attrName] = name;
+			return (
+				<ul className='dropdown-menu btn-block scrollable-menu'>
+					<li key={name}>
+						<a onClick={
+							() => { dispatch(dispatchParam); }
+						}>
+							{name}
+						</a>
+					</li>
+				</ul>
+			);
+		});
+
+		return (
+			<div className='form-group'>
+				{ buttonLabel ? <label>{buttonLabel}</label> : null }
+				<div className='btn-group btn-block'>
+					<button
+						type='button'
+						className='btn btn-block btn-default dropdown-toggle'
+						data-toggle='dropdown'
+						aria-haspopup='true'
+						aria-expanded='false' >
+						{ buttonName + '  '}
+						<span className='caret' />
+					</button>
+					{ options }
+				</div>
+			</div>
+		);
+	}
+}
+
+DropdownMenu.propTypes = {
+	buttonLabel: PropTypes.string,
+	buttonName: PropTypes.string.isRequired,
+	attributes: Proptypes.object.isRequired,
+	attrType: PropTypes.string.isRequired,
+	attrName: PropTypes.string.isRequired,
+	dispatch: PropTypes.func.isRequired,
+};
--- js/genescape-sidepanel.js
+++ js/genescape-sidepanel.js
@@ -1,93 +1,49 @@
 import React, { Component, PropTypes } from 'react';
+import { DropdownMenu } from './dropdown';
 
 export class GenescapeSidepanel extends Component {
 	render() {
-		const dispatch = this.props.dispatch;
-		const gs = this.props.genescapeState;
-		const ds = this.props.dataState;
+		const {dispatch, genescapeState, dataState } = this.props;
+		const rowAttrKeys = Object.keys(dataState.currentDataset.rowAttrs).sort();
 
-		const rowAttrKeys = Object.keys(ds.currentDataset.rowAttrs).sort();
-
-		const newOptions = (sortedKeys, typeLabel, nameLabel) => {
-			return (
-				sortedKeys.map((name) => {
-
-					let dispatchParam = { type: typeLabel };
-					dispatchParam[nameLabel] = name;
-
-					return (
-						<ul className='dropdown-menu btn-block scrollable-menu'>
-							<li key={name}>
-								<a onClick={
-									() => { dispatch(dispatchParam); }
-								}>
-									{name}
-								</a>
-							</li>
-						</ul>
-					);
-				})
-			);
-		};
-
-
-		const xOptions = newOptions(rowAttrKeys, 'SET_GENESCAPE_PROPS', 'xCoordinate');
-		const yOptions = newOptions(rowAttrKeys, 'SET_GENESCAPE_PROPS', 'yCoordinate');
-		const colorOptions = newOptions(rowAttrKeys, 'SET_GENESCAPE_PROPS', 'colorAttr');
-
-		const colorModes = (
-			<ul className='dropdown-menu'>
-				<li key='Categorical'>
-					<a onClick={
-						() => { dispatch({ type: 'SET_GENESCAPE_PROPS', colorMode: 'Categorical' }); }
-					}>
-						Categorical
-					</a>
-				</li>
-				<li key='Heatmap'>
-					<a onClick={
-						() => { dispatch({ type: 'SET_GENESCAPE_PROPS', colorMode: 'Heatmap' }); }
-					}>
-						Quantitative
-					</a>
-				</li>
-			</ul>
-		);
-
-		const newButtonmenu = (labelString, buttonName, options) => {
-			return (
-				<div className='form-group'>
-					{ labelString ? <label>{labelString}</label> : null }
-					<div className='btn-group btn-block'>
-						<button
-							type='button'
-							className='btn btn-block btn-default dropdown-toggle'
-							data-toggle='dropdown'
-							aria-haspopup='true'
-							aria-expanded='false' >
-							{ buttonName + '  '}
-							<span className='caret' />
-						</button>
-						{ options }
-					</div>
-				</div>
-			);
-		};
-
-		const xButton = newButtonmenu('X Coordinate', gs.xCoordinate, xOptions);
-		const yButton = newButtonmenu('Y Coordinate', gs.yCoordinate, yOptions);
-		const colorButton = newButtonmenu('Color', gs.colorAttr, colorOptions);
-		const colorModeButton = newButtonmenu(null, gs.colorMode, colorModes);
-
+		// TODO: If dropdown menu works here, refactor into heatmap-sidepanel.js
 		return (
 			<div className='panel panel-default'>
 				<div className='panel-heading'><h3 className='panel-title'>Settings</h3></div>
 				<div className='panel-body'>
 					<form>
-						{xButton}
-						{yButton}
-						{colorButton}
-						{colorModeButton}
+						<DropdownMenu
+							buttonLabel={'X Coordinate'}
+							buttonName={genescapeState.xCoordinate}
+							attributes={rowAttrKeys}
+							attrType={'SET_GENESCAPE_PROPS'}
+							attrName={'xCoordinate'}
+							dispatch={dispatch}
+							/>
+						<DropdownMenu
+							buttonLabel={'Y Coordinate'}
+							buttonName={genescapeState.yCoordinate}
+							attributes={rowAttrKeys}
+							attrType={'SET_GENESCAPE_PROPS'}
+							attrName={'yCoordinate'}
+							dispatch={dispatch}
+							/>
+						<DropdownMenu
+							buttonLabel={'Color'}
+							buttonName={genescapeState.colorAttr}
+							attributes={rowAttrKeys}
+							attrType={'SET_GENESCAPE_PROPS'}
+							attrName={'colorAttr'}
+							dispatch={dispatch}
+							/>
+						<DropdownMenu
+							buttonLabel={undefined}
+							buttonName={genescapeState.colorMode}
+							attributes={['Categorical', 'Heatmap']}
+							attrType={'SET_GENESCAPE_PROPS'}
+							attrName={'colorMode'}
+							dispatch={dispatch}
+							/>
 					</form>
 				</div>
 			</div>
