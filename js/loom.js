37409730c41b3be64fd9630008ede428c067f17d
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-05-17 12:37:49 +0200

    loom.js linter pass

--- js/loom.js
+++ js/loom.js
@@ -1,10 +1,10 @@
 
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import { Provider, connect } from 'react-redux';
 import thunk from 'redux-thunk';
-import loomAppReducer from './reducers'
+import loomAppReducer from './reducers';
 import { Navbar } from './navbar';
 import { DatasetView } from './dataset-view';
 import { HeatmapView } from './heatmap-view';
@@ -18,24 +18,26 @@ let store = applyMiddleware(thunk)(createStore)(loomAppReducer);
 
 
 class App extends Component {
-   	componentDidMount() {
-   		var dispatch = this.props.dispatch;
-		window.addEventListener("resize", ()=>{
+	componentDidMount() {
+		const dispatch = this.props.dispatch;
+		window.addEventListener("resize", () => {
 			dispatch(
 				{
-					type: 'SET_VIEW_PROPS', 
-					width: document.getElementById("react-root").clientWidth, 
-					height: window.innerHeight - document.getElementById("react-root").offsetTop
+					type: 'SET_VIEW_PROPS',
+					width: document.getElementById("react-root").clientWidth,
+					height: window.innerHeight - document.getElementById("react-root").offsetTop,
 				}
 			);
 		});
 		dispatch(fetchProjects());
 	}
+
 	componentWillUnmount() {
 		// Should remove the resize event listener here, but since this component will never unmount, we don't bother
 		// Because if we did bother, we'd need to figure out how to use 'this' in an event handler (again)
 	}
-  	render() {
+
+	render() {
 		// Injected by connect() call:
 		const dispatch = this.props.dispatch;
 		const ds = this.props.dataState;
@@ -45,70 +47,66 @@ class App extends Component {
 		const gs = this.props.genescapeState;
 		const vs = this.props.viewState;
 
-		var view = <div></div>;
+		let view = <div></div>;
 
-		switch(this.props.viewState.view) {
-			case "Dataset":
-				view =
-					<DatasetView 
+		switch (this.props.viewState.view) {
+		case "Dataset":
+			view =
+					<DatasetView
 						viewState={vs}
 						dataState={ds}
 						heatmapState={hs}
 						dispatch={dispatch}
-					/>
-				break;
-			case "Heatmap":
-				view =
-					<HeatmapView 
+						/>;
+			break;
+		case "Heatmap":
+			view =
+					<HeatmapView
 						viewState={vs}
 						dataState={ds}
 						heatmapState={hs}
 						dispatch={dispatch}
-					/>
-				break;
-			case "Sparkline":
-				view =
-					<SparklineView 
+						/>;
+			break;
+		case "Sparkline":
+			view =
+					<SparklineView
 						viewState={vs}
 						dataState={ds}
 						sparklineState={ss}
 						dispatch={dispatch}
-					/>
-				break;
-			case "Landscape":
-				view = <LandscapeView
+						/>;
+			break;
+		case "Landscape":
+			view = <LandscapeView
 					viewState={vs}
 					dataState={ds}
 					landscapeState={ls}
 					dispatch={dispatch}
-				/>
-				break;
-			case "Genescape":
-				view = <GenescapeView
+					/>;
+			break;
+		case "Genescape":
+			view = <GenescapeView
 					viewState={vs}
 					dataState={ds}
 					genescapeState={gs}
 					dispatch={dispatch}
-				/>
-				break;
-			default:
-				view = <div>{"Unknown view: " + this.props.viewState.view}</div>;		
-				break;
+					/>;
+			break;
+		default:
+			view = <div>{"Unknown view: " + this.props.viewState.view}</div>;
+			break;
 		}
 		return (
 			<div>
-				<Navbar 
+				<Navbar
 					viewState={vs}
 					dataState={ds}
-					onSetViewState={(state)=>
-						{
-							if(ds.hasDataset) {
-								dispatch({type: 'SET_VIEW_PROPS', view: state});
-							}
+					onSetViewState={
+						(state) => {
+							ds.hasDataset ? dispatch({ type: 'SET_VIEW_PROPS', view: state }) : undefined;
 						}
-					}
-
-				/>
+					}/>
 				{view}
 			</div>
 		);
@@ -119,22 +117,23 @@ App.propTypes = {
 	heatmapState: PropTypes.object,
 	landscapeState: PropTypes.object,
 	genescapeState: PropTypes.object,
-	viewState: PropTypes.object
-}
+	viewState: PropTypes.object,
+};
 
 // Which props do we want to inject, given the global state?
 // Note: use https://github.com/faassen/reselect for better performance.
 function select(state) {
-	return state
+	return state;
 }
 
 // Wrap the component to inject dispatch and state into it
-App = connect(select)(App)
+let LoomApp = connect(select)(App);
 
 
 
 render(
 	<Provider store={store}>
-		<App/>
+		<LoomApp />
 	</Provider>
-, document.getElementById('react-root'));
+	, document.getElementById('react-root')
+);
