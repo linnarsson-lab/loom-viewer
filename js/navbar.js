ad6596ccfa3fbf7801656ff7855cb7e02408dccd
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-06-22 15:30:16 +0200

    navbar.js linter pass

--- js/navbar.js
+++ js/navbar.js
@@ -2,38 +2,45 @@ import React, { Component, PropTypes } from 'react';
 import { setViewState } from './actions';
 
 export class Navbar extends Component {
-  render() {
-  	var vs = this.props.viewState;
-  	var ds = this.props.dataState;
+	render() {
+		const {viewState, dataState, onSetViewState } = this.props;
+		const view = viewState.view;
 
-	return(
-		<nav className="navbar navbar-default navbar-fixed-top">
-		  <div className="container">
-			<div className="navbar-header">
-			  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-				<span className="sr-only">Toggle navigation</span>
-				<span className="icon-bar"></span>
-				<span className="icon-bar"></span>
-				<span className="icon-bar"></span>
-			  </button>
-			  <span className="navbar-brand">{ds.currentDataset.name}</span>
-			</div>
-			<div id="navbar" className="collapse navbar-collapse">
-			  <ul className="nav navbar-nav">
-				<li className={vs.view == "Dataset" ? "active" : ""}><a href="#" onClick={(event)=>this.props.onSetViewState('Dataset')}>Dataset</a></li>
-				<li className={vs.view == "Heatmap" ? "active" : ""}><a href="#" onClick={(event)=>this.props.onSetViewState('Heatmap')}>Heatmap</a></li>
-				<li className={vs.view == "Landscape" ? "active" : ""}><a href="#" onClick={(event)=>this.props.onSetViewState('Landscape')}>Landscape</a></li>
-				<li className={vs.view == "Sparkline" ? "active" : ""}><a href="#" onClick={(event)=>this.props.onSetViewState('Sparkline')}>Sparkline</a></li>
-				<li className={vs.view == "Genescape" ? "active" : ""}><a href="#" onClick={(event)=>this.props.onSetViewState('Genescape')}>Genescape</a></li>
-			  </ul>
-			</div>
-		  </div>
-		</nav>      
-  )}
+		return (
+			<nav className='navbar navbar-default navbar-fixed-top'>
+				<div className='container'>
+					<div className='navbar-header'>
+						<button
+							type='button'
+							className='navbar-toggle collapsed'
+							data-toggle='collapse'
+							data-target='#navbar'
+							aria-expanded='false'
+							aria-controls='navbar' >
+							<span className='sr-only'>Toggle navigation</span>
+							<span className='icon-bar'></span>
+							<span className='icon-bar'></span>
+							<span className='icon-bar'></span>
+						</button>
+						<span className='navbar-brand'>{dataState.currentDataset.name}</span>
+					</div>
+					<div id='navbar' className='collapse navbar-collapse'>
+						<ul className='nav navbar-nav'>
+							<li className={ view === 'Dataset' ? 'active' : ''}><a href='#' onClick={ () => { onSetViewState('Dataset'); } }>Dataset</a></li>
+							<li className={ view === 'Heatmap' ? 'active' : ''}><a href='#' onClick={ () => { onSetViewState('Heatmap'); } }>Heatmap</a></li>
+							<li className={ view === 'Landscape' ? 'active' : ''}><a href='#' onClick={ () => { onSetViewState('Landscape'); } }>Landscape</a></li>
+							<li className={ view === 'Sparkline' ? 'active' : ''}><a href='#' onClick={ () => { onSetViewState('Sparkline'); } }>Sparkline</a></li>
+							<li className={ view === 'Genescape' ? 'active' : ''}><a href='#' onClick={ () => { onSetViewState('Genescape'); } }>Genescape</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 }
 
 Navbar.propTypes = {
 	viewState: PropTypes.object.isRequired,
 	dataState: PropTypes.object.isRequired,
-	onSetViewState: PropTypes.func.isRequired
+	onSetViewState: PropTypes.func.isRequired,
 }
