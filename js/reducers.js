5f3d386232d417a94e62af05aaea1f4c61434dbb
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-06-22 14:49:25 +0200

    reducers linter pass

--- js/reducers.js
+++ js/reducers.js
@@ -4,102 +4,97 @@ import { combineReducers } from 'redux';
 
 const initialViewState = {
 	type: "SET_VIEW_PROPS",	// This will be set to the last action type
-
 	view: 'Dataset',
 	width: document.getElementById("react-root").clientWidth,
-	height: window.innerHeight - document.getElementById("react-root").offsetTop - 50 // For some reason, at launch the height is 50px off (=navbar height)
+	height: window.innerHeight - document.getElementById("react-root").offsetTop - 50, // For some reason, at launch the height is 50px off (=navbar height)
 };
 
-function viewState(state=initialViewState, action) {
+function viewState(state = initialViewState, action) {
 	switch (action.type) {
-		case 'SET_VIEW_PROPS':
-			return Object.assign({}, state,	action);	// NOTE: must start with an empty object {} to ensure we don't mutate state
-		default:
-			return state;
+	case 'SET_VIEW_PROPS':
+		return Object.assign({}, state, action);	// NOTE: must start with an empty object {} to ensure we don't mutate state
+	default:
+		return state;
 	}
 }
 
 const initialSparklineState = {
 	type: "SET_SPARKLINE_PROPS",	// This will be set to the last action type
-
 	colAttr: "CellID",
 	colMode: "Text",
 	orderByAttr: "(none)",	// meaning, original order
 	orderByGene: "",
 	geneMode: "Heatmap",
-	genes: ""
-}
+	genes: "",
+};
 
-function sparklineState(state=initialSparklineState, action) {
+function sparklineState(state = initialSparklineState, action) {
 	switch (action.type) {
-		case 'SET_SPARKLINE_PROPS':
-			return Object.assign({}, state,	action);
-		default:
-			return state;
+	case 'SET_SPARKLINE_PROPS':
+		return Object.assign({}, state, action);
+	default:
+		return state;
 	}
 }
 
 
 const initialLandscapeState = {
 	type: "SET_LANDSCAPE_PROPS",	// This will be set to the last action type
-
 	xCoordinate: "_tSNE1",
 	xGene: "",
 	yCoordinate: "_tSNE2",
 	yGene: "",
 	colorAttr: "CellID",
 	colorMode: "Heatmap",
-	colorGene: ""
+	colorGene: "",
 };
 
-function landscapeState(state=initialLandscapeState, action) {
+function landscapeState(state = initialLandscapeState, action) {
 	switch (action.type) {
-		case 'SET_LANDSCAPE_PROPS':
-			return Object.assign({}, state,	action);
-		default:
-			return state;
+	case 'SET_LANDSCAPE_PROPS':
+		return Object.assign({}, state, action);
+	default:
+		return state;
 	}
 }
 
 const initialGenescapeState = {
 	type: "SET_GENESCAPE_PROPS",	// This will be set to the last action type
-
 	xCoordinate: "",
 	yCoordinate: "",
 	colorAttr: "",
-	colorMode: "Heatmap"
+	colorMode: "Heatmap",
 };
 
-function genescapeState(state=initialGenescapeState, action) {
+function genescapeState(state = initialGenescapeState, action) {
 	switch (action.type) {
-		case 'SET_GENESCAPE_PROPS':
-			return Object.assign({}, state,	action);
-		default:
-			return state;
+	case 'SET_GENESCAPE_PROPS':
+		return Object.assign({}, state, action);
+	default:
+		return state;
 	}
 }
 
 const initialHeatmapState = {
 	type: 'SET_HEATMAP_PROPS',	// This prop gets set by the reducer below, but we should ignore it
-
-	screenBounds: (0,0,0,0),	// Screen pixel coordinates of the dataset in the current view
-	dataBounds: (0,0,0,0),		// Data coordinates of the current view
-	center: L.latLng(0,0),
+	screenBounds: (0, 0, 0, 0),	// Screen pixel coordinates of the dataset in the current view
+	dataBounds: (0, 0, 0, 0),		// Data coordinates of the current view
+	center: L.latLng(0, 0),
 	zoom: 8,
 	rowAttr: "",
 	rowMode: 'Text',
 	rowGenes: '',
 	colAttr: "",
 	colMode: 'Text',
-	colGene: ''
+	colGene: '',
 };
 
-function heatmapState(state=initialHeatmapState, action) {
+function heatmapState(state = initialHeatmapState, action) {
 	switch (action.type) {
-		case 'SET_HEATMAP_PROPS':
-			return Object.assign({}, state,	action);
-		default:
-			return state;
+	case 'SET_HEATMAP_PROPS':
+		return Object.assign({}, state, action);
+	default:
+		return state;
 	}
 }
 
@@ -112,46 +107,46 @@ const initialDataState = {
 	hasDataset: false,
 
 	currentDataset: {},			// rowAttrs, colAttrs and such things, when loaded; replaces 'window.fileinfo'
-	genes: {}					// contains row data by gene, i.e. {"Actb": [1,2,1,3,42,4,...]}
+	genes: {},					// contains row data by gene, i.e. {"Actb": [1,2,1,3,42,4,...]}
 };
 
-function dataState(state=initialDataState, action) {
+function dataState(state = initialDataState, action) {
 	switch (action.type) {
-		case 'REQUEST_PROJECTS':
-			return Object.assign({}, state,	{isFetchingData: true, errorFetchingData: false});
-		case 'RECEIVE_PROJECTS':
-			return Object.assign({}, state,	{
-				isFetchingData: false,
-				projects: action.projects
-			});
-		case 'REQUEST_PROJECTS_FAILED':
-			return Object.assign({}, state,	{isFetchingData: false, errorFetchingData: true});
+	case 'REQUEST_PROJECTS':
+		return Object.assign({}, state, { isFetchingData: true, errorFetchingData: false });
+	case 'RECEIVE_PROJECTS':
+		return Object.assign({}, state, {
+			isFetchingData: false,
+			projects: action.projects,
+		});
+	case 'REQUEST_PROJECTS_FAILED':
+		return Object.assign({}, state, { isFetchingData: false, errorFetchingData: true });
 
-		case 'REQUEST_DATASET':
-			return Object.assign({}, state,	{isFetchingData: true, errorFetchingData: false});
-		case 'RECEIVE_DATASET':
-			return Object.assign({}, state,	{
-				isFetchingData: false,
-				hasDataset: true,
-				genes: {},
-				currentDataset: action.dataset
-			});
-		case 'REQUEST_DATASET_FAILED':
-			return Object.assign({}, state,	{isFetchingData: false, errorFetchingData: true});
+	case 'REQUEST_DATASET':
+		return Object.assign({}, state, { isFetchingData: true, errorFetchingData: false });
+	case 'RECEIVE_DATASET':
+		return Object.assign({}, state, {
+			isFetchingData: false,
+			hasDataset: true,
+			genes: {},
+			currentDataset: action.dataset,
+		});
+	case 'REQUEST_DATASET_FAILED':
+		return Object.assign({}, state, { isFetchingData: false, errorFetchingData: true });
 
-		case 'REQUEST_GENE':
-			return Object.assign({}, state,	{isFetchingData: true, errorFetchingData: false});
-		case 'RECEIVE_GENE':
-			return Object.assign({}, state,	{
-				isFetchingData: false,
-				genes: Object.assign({}, state.genes, {
-					[action.gene]: action.data
-				})
-			});
-		case 'REQUEST_GENE_FAILED':
-			return Object.assign({}, state,	{isFetchingData: false, errorFetchingData: true});
-		default:
-			return state;
+	case 'REQUEST_GENE':
+		return Object.assign({}, state, { isFetchingData: true, errorFetchingData: false });
+	case 'RECEIVE_GENE':
+		return Object.assign({}, state, {
+			isFetchingData: false,
+			genes: Object.assign({}, state.genes, {
+				[action.gene]: action.data,
+			}),
+		});
+	case 'REQUEST_GENE_FAILED':
+		return Object.assign({}, state, { isFetchingData: false, errorFetchingData: true });
+	default:
+		return state;
 	}
 }
 
@@ -161,7 +156,7 @@ const loomAppReducer = combineReducers({
 	landscapeState,
 	genescapeState,
 	sparklineState,
-	dataState
+	dataState,
 });
 
 export default loomAppReducer;
