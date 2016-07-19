7b88adbd4d7fc6401a24599cabe6236f840a2559
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-07-19 11:44:03 +0200

    MainView linter pass

--- client/components/main-view.js
+++ client/components/main-view.js
@@ -1,32 +1,30 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 import { Grid, Col, Row, Jumbotron, Button, ButtonToolbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-export class MainView extends Component {
-	render() {
-		return (
-			<Grid>
-				<Row>
-					<Col xs={12} md={8}>
-						<Jumbotron>
-							<h1>Loom</h1>
-							<h2>Tool for browsing and visualizing single-cell data</h2>
-							<ButtonToolbar>
-								<LinkContainer to='/dataset'>
-									<Button bsStyle='primary'>
-										View Datasets
-									</Button>
-								</LinkContainer>
-								<LinkContainer to='/upload'>
-									<Button bsStyle='primary'>
-										Upload New Dataset
-									</Button>
-								</LinkContainer>
-							</ButtonToolbar>
-						</Jumbotron>
-					</Col>
-				</Row>
-			</Grid>
-		);
-	}
-}
+export const MainView = function () {
+	return (
+		<Grid>
+			<Row>
+				<Col xs={12} md={8}>
+					<Jumbotron>
+						<h1>Loom</h1>
+						<h2>Tool for browsing and visualizing single-cell data</h2>
+						<ButtonToolbar>
+							<LinkContainer to='/dataset'>
+								<Button bsStyle='primary'>
+									View Datasets
+								</Button>
+							</LinkContainer>
+							<LinkContainer to='/upload'>
+								<Button bsStyle='primary'>
+									Upload New Dataset
+								</Button>
+							</LinkContainer>
+						</ButtonToolbar>
+					</Jumbotron>
+				</Col>
+			</Row>
+		</Grid>
+	);
+};
