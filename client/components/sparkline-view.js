e8d171545e126a41d8601e9a3e25e5281583cc45
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-07-19 15:16:42 +0200

    sparkline-view linter psas

--- client/components/sparkline-view.js
+++ client/components/sparkline-view.js
@@ -7,45 +7,49 @@ import * as _ from 'lodash';
 
 export class SparklineView extends Component {
 	render() {
-		var ss = this.props.sparklineState;
-		var ds = this.props.dataState;
-		var vs = this.props.viewState;
-		var dispatch = this.props.dispatch;
+		const ss = this.props.sparklineState;
+		const ds = this.props.dataState;
+		const vs = this.props.viewState;
+		const dispatch = this.props.dispatch;
 
-		var colData = ds.currentDataset.colAttrs[ss.colAttr];
+		let colData = ds.currentDataset.colAttrs[ss.colAttr];
 		// Figure out the ordering
-		var indices = new Array(colData.length);
-		for (var i = 0; i < colData.length; ++i) indices[i] = i;
-		if (ss.orderByAttr != "(none)") {
-			var orderBy = null;
-			if (ss.orderByAttr == "(gene)") {
+		let indices = new Array(colData.length);
+		for (let i = 0; i < colData.length; ++i) {
+			indices[i] = i;
+		}
+		if (ss.orderByAttr !== "(none)") {
+			let orderBy = null;
+			if (ss.orderByAttr === "(gene)") {
 				if (ds.genes.hasOwnProperty(ss.orderByGene)) {
 					orderBy = ds.genes[ss.orderByGene];
 				}
 			} else {
 				orderBy = ds.currentDataset.colAttrs[ss.orderByAttr];
 			}
-			if (orderBy != null) {
-				indices.sort(function (a, b) { return orderBy[a] < orderBy[b] ? -1 : orderBy[a] > orderBy[b] ? 1 : 0; });
+			if (orderBy !== null) {
+				indices.sort((a, b) => { return orderBy[a] < orderBy[b] ? -1 : orderBy[a] > orderBy[b] ? 1 : 0; });
 			}
 		}
 
 		// Order the column attribute values
-		var temp = new Array(colData.length);
-		for (var i = 0; i < colData.length; ++i) temp[i] = colData[indices[i]];
+		let temp = new Array(colData.length);
+		for (let i = 0; i < colData.length; ++i) { temp[i] = colData[indices[i]]; }
 		colData = temp;
 
-		var genes = _.uniq(ss.genes.trim().split(/[ ,\r\n]+/));
-		if (genes.length > 0 && genes[0] != "") {
-			var geneSparklines = _.map(genes, (gene) => {
+		const genes = _.uniq(ss.genes.trim().split(/[ ,\r\n]+/));
+		const geneSparklines = (genes.length === 0 || genes[0] === "") ? <div></div> : (
+			_.map(genes, (gene) => {
 				if (ds.genes.hasOwnProperty(gene)) {
-					var geneData = new Array(colData.length);
-					for (var i = 0; i < geneData.length; ++i) geneData[i] = ds.genes[gene][indices[i]];
+					let geneData = new Array(colData.length);
+					for (let i = 0; i < geneData.length; ++i) {
+						geneData[i] = ds.genes[gene][indices[i]];
+					}
 
 					return (
 						<div key={gene}>
 							<Sparkline
-								orientation="horizontal"
+								orientation='horizontal'
 								width={vs.width - 450}
 								height={20}
 								data={geneData}
@@ -53,27 +57,26 @@ export class SparklineView extends Component {
 								screenRange={[0, vs.width - 450]}
 								mode={ss.geneMode}
 								/>
-							<span className="sparkline-label">{gene}</span>
+							<span className='sparkline-label'>{gene}</span>
 						</div>
 					);
 				} else {
 					return <div key={gene}></div>;
 				}
-			});
-		} else {
-			var geneSparklines = <div></div>;
-		}
+			})
+		);
+
 
 		return (
-			<div className="view">
-				<div className="view-sidepanel">
+			<div className='view'>
+				<div className='view-sidepanel'>
 					<SparklineSidepanel
 						sparklineState={ss}
 						dataState={ds}
 						dispatch={dispatch}
 						/>
 				</div>
-				<div className="view-main">
+				<div className='view-main'>
 					{/* Borrowing the Leaflet zoom buttons
 			  	<div className="leaflet-top leaflet-left">
 					<div className="leaflet-control-zoom leaflet-bar leaflet-control">
@@ -82,7 +85,7 @@ export class SparklineView extends Component {
 					</div>
 				</div>				*/}
 					<Sparkline
-						orientation="horizontal"
+						orientation='horizontal'
 						width={vs.width - 450}
 						height={20}
 						data={colData}
@@ -90,13 +93,15 @@ export class SparklineView extends Component {
 						screenRange={[0, vs.width - 450]}
 						mode={ss.colMode}
 						/>
-					<span className="sparkline-label">{ss.colAttr}</span>
+					<span className='sparkline-label'>
+						{ss.colAttr}
+					</span>
 					<div>
 						{geneSparklines}
 					</div>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
@@ -104,5 +109,5 @@ SparklineView.propTypes = {
 	viewState: PropTypes.object.isRequired,
 	dataState: PropTypes.object.isRequired,
 	sparklineState: PropTypes.object.isRequired,
-	dispatch: PropTypes.func.isRequired
-}
+	dispatch: PropTypes.func.isRequired,
+};
