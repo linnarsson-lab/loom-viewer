48cd477e5da1832c6b2312ced3a830f0392648d8
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-07-19 15:37:58 +0200

    dropdown.js linter pass

--- client/components/dropdown.js
+++ client/components/dropdown.js
@@ -1,50 +1,47 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
-export class DropdownMenu extends Component {
+export const DropdownMenu = function (props) {
 
-	render() {
+	const {
+		buttonLabel, buttonName,
+		attributes, attrType, attrName,
+		dispatch,
+	} = props;
 
-		const {
-			buttonLabel, buttonName,
-			attributes, attrType, attrName,
-			dispatch,
-		} = this.props;
-
-		const options = attributes.map((name) => {
-			let dispatchParam = { type: attrType };
-			dispatchParam[attrName] = name;
-			return (
-				<ul className='dropdown-menu btn-block scrollable-menu'>
-					<li key={name}>
-						<a onClick={
-							() => { dispatch(dispatchParam); }
-						}>
-							{name}
-						</a>
-					</li>
-				</ul>
-			);
-		});
-
+	const options = attributes.map((name) => {
+		let dispatchParam = { type: attrType };
+		dispatchParam[attrName] = name;
 		return (
-			<div className='form-group'>
-				{ buttonLabel ? <label>{buttonLabel}</label> : null }
-				<div className='btn-group btn-block'>
-					<button
-						type='button'
-						className='btn btn-block btn-default dropdown-toggle'
-						data-toggle='dropdown'
-						aria-haspopup='true'
-						aria-expanded='false' >
-						{ buttonName + '  '}
-						<span className='caret' />
-					</button>
-					{ options }
-				</div>
-			</div>
+			<ul className='dropdown-menu btn-block scrollable-menu'>
+				<li key={name}>
+					<a onClick={
+						() => { dispatch(dispatchParam); }
+					}>
+						{name}
+					</a>
+				</li>
+			</ul>
 		);
-	}
-}
+	});
+
+	return (
+		<div className='form-group'>
+			{ buttonLabel ? <label>{buttonLabel}</label> : null }
+			<div className='btn-group btn-block'>
+				<button
+					type='button'
+					className='btn btn-block btn-default dropdown-toggle'
+					data-toggle='dropdown'
+					aria-haspopup='true'
+					aria-expanded='false' >
+					{ buttonName + '  '}
+					<span className='caret' />
+				</button>
+				{ options }
+			</div>
+		</div>
+	);
+};
 
 DropdownMenu.propTypes = {
 	buttonLabel: PropTypes.string,
