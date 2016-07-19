e02620e1350b548588f490e0da97cfc4f581c778
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-07-19 11:46:47 +0200

    GenescapeSidepanel linter pass

--- client/components/genescape-sidepanel.js
+++ client/components/genescape-sidepanel.js
@@ -1,55 +1,53 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { DropdownMenu } from './dropdown';
 
-export class GenescapeSidepanel extends Component {
-	render() {
-		const { dispatch, genescapeState, dataState } = this.props;
-		const rowAttrKeys = Object.keys(dataState.currentDataset.rowAttrs).sort();
+export const GenescapeSidepanel = function (props) {
+	const { dispatch, genescapeState, dataState } = props;
+	const rowAttrKeys = Object.keys(dataState.currentDataset.rowAttrs).sort();
 
-		// TODO: If dropdown menu works here, refactor into heatmap-sidepanel.js
-		return (
-			<div className='panel panel-default'>
-				<div className='panel-heading'><h3 className='panel-title'>Settings</h3></div>
-				<div className='panel-body'>
-					<form>
-						<DropdownMenu
-							buttonLabel={'X Coordinate'}
-							buttonName={genescapeState.xCoordinate}
-							attributes={rowAttrKeys}
-							attrType={'SET_GENESCAPE_PROPS'}
-							attrName={'xCoordinate'}
-							dispatch={dispatch}
-							/>
-						<DropdownMenu
-							buttonLabel={'Y Coordinate'}
-							buttonName={genescapeState.yCoordinate}
-							attributes={rowAttrKeys}
-							attrType={'SET_GENESCAPE_PROPS'}
-							attrName={'yCoordinate'}
-							dispatch={dispatch}
-							/>
-						<DropdownMenu
-							buttonLabel={'Color'}
-							buttonName={genescapeState.colorAttr}
-							attributes={rowAttrKeys}
-							attrType={'SET_GENESCAPE_PROPS'}
-							attrName={'colorAttr'}
-							dispatch={dispatch}
-							/>
-						<DropdownMenu
-							buttonLabel={undefined}
-							buttonName={genescapeState.colorMode}
-							attributes={['Categorical', 'Heatmap']}
-							attrType={'SET_GENESCAPE_PROPS'}
-							attrName={'colorMode'}
-							dispatch={dispatch}
-							/>
-					</form>
-				</div>
+	// TODO: If dropdown menu works here, refactor into heatmap-sidepanel.js
+	return (
+		<div className='panel panel-default'>
+			<div className='panel-heading'><h3 className='panel-title'>Settings</h3></div>
+			<div className='panel-body'>
+				<form>
+					<DropdownMenu
+						buttonLabel={'X Coordinate'}
+						buttonName={genescapeState.xCoordinate}
+						attributes={rowAttrKeys}
+						attrType={'SET_GENESCAPE_PROPS'}
+						attrName={'xCoordinate'}
+						dispatch={dispatch}
+						/>
+					<DropdownMenu
+						buttonLabel={'Y Coordinate'}
+						buttonName={genescapeState.yCoordinate}
+						attributes={rowAttrKeys}
+						attrType={'SET_GENESCAPE_PROPS'}
+						attrName={'yCoordinate'}
+						dispatch={dispatch}
+						/>
+					<DropdownMenu
+						buttonLabel={'Color'}
+						buttonName={genescapeState.colorAttr}
+						attributes={rowAttrKeys}
+						attrType={'SET_GENESCAPE_PROPS'}
+						attrName={'colorAttr'}
+						dispatch={dispatch}
+						/>
+					<DropdownMenu
+						buttonLabel={undefined}
+						buttonName={genescapeState.colorMode}
+						attributes={['Categorical', 'Heatmap']}
+						attrType={'SET_GENESCAPE_PROPS'}
+						attrName={'colorMode'}
+						dispatch={dispatch}
+						/>
+				</form>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 GenescapeSidepanel.propTypes = {
 	genescapeState: PropTypes.object.isRequired,
