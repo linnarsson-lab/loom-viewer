3f2a7adc3b2061984e6b9be41ee1aef2e22835eb
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-07-19 15:16:48 +0200

    landscape-view linter pass

--- client/components/landscape-view.js
+++ client/components/landscape-view.js
@@ -5,9 +5,9 @@ import { Scatterplot } from './scatterplot';
 
 export class LandscapeView extends Component {
 	makeData(attr, gene) {
-		var data = [];
-		if(attr == "(gene)") {
-			if(this.props.dataState.genes.hasOwnProperty(gene)) {
+		let data = [];
+		if (attr === "(gene)") {
+			if (this.props.dataState.genes.hasOwnProperty(gene)) {
 				data = this.props.dataState.genes[gene];
 			}
 		} else {
@@ -16,38 +16,38 @@ export class LandscapeView extends Component {
 		return data;
 	}
 	render() {
-		var dispatch = this.props.dispatch;
-		var ls = this.props.landscapeState;
-		var ds = this.props.dataState;
-	  	var vs = this.props.viewState;
+		const dispatch = this.props.dispatch;
+		const ls = this.props.landscapeState;
+		const ds = this.props.dataState;
+		const vs = this.props.viewState;
 
-		var color = this.makeData(ls.colorAttr, ls.colorGene);
-		var x = this.makeData(ls.xCoordinate, ls.xGene);
-		var y = this.makeData(ls.yCoordinate, ls.yGene);
+		const color = this.makeData(ls.colorAttr, ls.colorGene);
+		const x = this.makeData(ls.xCoordinate, ls.xGene);
+		const y = this.makeData(ls.yCoordinate, ls.yGene);
 		return (
-		<div className="view">
-			<div className="view-sidepanel">
-				<LandscapeSidepanel
-					landscapeState={ls}
-					dataState={ds}
-					dispatch={dispatch}
-				/>
+			<div className='view'>
+				<div className='view-sidepanel'>
+					<LandscapeSidepanel
+						landscapeState={ls}
+						dataState={ds}
+						dispatch={dispatch}
+						/>
+				</div>
+				<div className='view-main'>
+					<Scatterplot
+						x={x}
+						y={y}
+						color={color}
+						colorMode={ls.colorMode}
+						width={vs.width - 350}
+						height={vs.height - 40}
+						logScaleColor={ls.colorAttr === "(gene)"}
+						logScaleX={ls.xCoordinate === "(gene)"}
+						logScaleY={ls.yCoordinate === "(gene)"}
+						/>
+				</div>
 			</div>
-			<div className="view-main">
-				<Scatterplot
-					x={x}
-					y={y}
-					color={color}
-					colorMode={ls.colorMode}
-					width={vs.width - 350}
-					height={vs.height - 40}
-					logScaleColor={ls.colorAttr == "(gene)"}
-					logScaleX={ls.xCoordinate == "(gene)"}
-					logScaleY={ls.yCoordinate == "(gene)"}
-				/>
-			</div>
-		</div>
-		)
+		);
 	}
 }
 
@@ -55,5 +55,5 @@ LandscapeView.propTypes = {
 	viewState: PropTypes.object.isRequired,
 	dataState: PropTypes.object.isRequired,
 	landscapeState: PropTypes.object.isRequired,
-	dispatch: PropTypes.func.isRequired
-}
+	dispatch: PropTypes.func.isRequired,
+};
