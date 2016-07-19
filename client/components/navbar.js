ba3f56b7ab939fac63f227cb4e38ffe48574398e
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-07-19 11:43:36 +0200

    NavbarView linter pass

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,38 +1,38 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-export class NavbarView extends Component {
-	render() {
-		const navbarInstance = (
-			<Navbar>
-				<Navbar.Header>
-					<LinkContainer to='/'>
-						<Navbar.Brand>
-							Loom
-						</Navbar.Brand>
-					</LinkContainer>
-					<Navbar.Toggle />
-				</Navbar.Header>
-				<Navbar.Collapse>
-					<Nav>
-						<LinkContainer to='/dataset'><NavItem eventKey={1}>
-							Dataset
-						</NavItem></LinkContainer>
-						<LinkContainer to='/upload'><NavItem eventKey={2}>
-							Upload
-						</NavItem></LinkContainer>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		);
-		return (
-			<div>
-				{navbarInstance}
-				{this.props.children}
-			</div>
-		);
-	}
-}
+export const NavbarView = function (props) {
+	const navbarInstance = (
+		<Navbar>
+			<Navbar.Header>
+				<LinkContainer to='/'>
+					<Navbar.Brand>
+						Loom
+					</Navbar.Brand>
+				</LinkContainer>
+				<Navbar.Toggle />
+			</Navbar.Header>
+			<Navbar.Collapse>
+				<Nav>
+					<LinkContainer to='/dataset'><NavItem eventKey={1}>
+						Dataset
+					</NavItem></LinkContainer>
+					<LinkContainer to='/upload'><NavItem eventKey={2}>
+						Upload
+					</NavItem></LinkContainer>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+	return (
+		<div>
+			{navbarInstance}
+			{props.children}
+		</div>
+	);
+};
 
-Navbar.propTypes = {};
+NavbarView.propTypes = {
+	children: PropTypes.node,
+};
