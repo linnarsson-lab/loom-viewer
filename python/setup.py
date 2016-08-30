4ce37bb7b82a5e0f5fdec0480c00d2b0e132f7e1
Sten Linnarsson <sten.linnarsson@ki.se>
2016-08-30 17:16:23 +0200

    one more try

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ setup(
         'scikit-learn',
         'h5py', 
         'pandas', 
-        'scipy==0.17.1',
+        'scipy<=0.17.1',
         'numpy', 
         'progressbar',
         'requests',
