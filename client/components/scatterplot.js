87afdc244a158914e6f129dc9795e2832ed5bae9
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-09-06 13:08:26 +0200

    Scatterplot scales again

--- client/components/scatterplot.js
+++ client/components/scatterplot.js
@@ -10,11 +10,9 @@ export class Scatterplot extends React.Component {
 
 	constructor(props) {
 		super(props);
-
-		// Required to let us pass `paint` to a
-		// Canvas component without `this` errors
 		this.paint = this.paint.bind(this);
 	}
+
 
 	paint(context) {
 		if (this.props.x === undefined ||
@@ -22,7 +20,7 @@ export class Scatterplot extends React.Component {
 			this.props.color === undefined) {
 			return;
 		}
-		let { width, height } = context;
+		let { width, height, pixelRatio } = context;
 
 		// Erase previous paint
 		context.save();
@@ -32,25 +30,45 @@ export class Scatterplot extends React.Component {
 		// Calculate some general properties
 		// Make room for color legend on right
 		width = (width - 200);
+
 		let x = this.props.x;
-		// Log transform if requested
-		if (this.props.logScaleX) {
-			x = x.map((x) => { return Math.log2(x + 1 + 0.5 * Math.random()); });
-		}
-		x = _.map(x, (t) => { return isFinite(t) ? t : 0; });
 		let y = this.props.y;
 		// Log transform if requested
-		if (this.props.logScaleY) {
-			y = y.map((x) => { return Math.log2(x + 1 + 0.5 * Math.random()); });
+		if (this.props.logScaleX) {
+			for (let i = 0; i < x.length; i++) {
+				x[i] = Math.log2(x + Math.random() - 0.5);
+			}
 		}
-		y = _.map(y, (t) => { return isFinite(t) ? t : 0; });
+		if (this.props.logScaleY) {
+			for (let i = 0; i < y.length; i++) {
+				y[i] = Math.log2(y + Math.random() - 0.5);
+			}
+		}
+
 		// Suitable radius of the markers
-		const radius = Math.max(3, Math.sqrt(x.length) / 60);
+		const radius = Math.max(3, Math.sqrt(x.length) / 60) * pixelRatio;
+
 		// Scale of data
 		const xmin = Math.min(...x);
 		const xmax = Math.max(...x);
 		const ymin = Math.min(...y);
 		const ymax = Math.max(...y);
+
+
+		let xScaled = x.slice(0);
+		let yScaled = y.slice(0);
+		for (let i = 0; i < x.length; i++) {
+			const xi = (x[i] - xmin) / (xmax - xmin) * (width - 2 * radius) + radius;
+			xScaled[i] = xi | 0;
+		}
+		for (let i = 0; i < y.length; i++) {
+			// "1-" because Y needs to be flipped
+			const yi = (1 - (y[i] - ymin) / (ymax - ymin)) * (height - 2 * radius) + radius;
+			yScaled[i] = yi | 0;
+		}
+		x = xScaled;
+		y = yScaled;
+
 		let color = this.props.color;
 		const palette = (this.props.colorMode === 'Heatmap' ? colors.solar9 : colors.category20);
 
@@ -59,34 +77,39 @@ export class Scatterplot extends React.Component {
 			color = Array.from({ length: x.length }, () => { return 'grey'; });
 		} else {
 			// Do we need to categorize the color scale?
-			if (this.props.colorMode === 'Categorical' || !_.every(color, (x) => { return isFinite(x); })) {
+			if (this.props.colorMode === 'Categorical' || !_.every(color, (c) => { return isFinite(c); })) {
 
 				// Reserve palette[0] for all uncategorized items
 				let cats = nMostFrequent(color, palette.length - 1);
 
 				// Add one so the uncategorized become zero
-				color = color.map((x) => { return palette[cats.indexOf(x) + 1]; });
+				color = color.map((c) => { return palette[cats.indexOf(c) + 1]; });
 
 				// Draw the figure legend
 				// Start at -1 which corresponds to
 				// the "(other)" category, i.e. those
 				// that didn't fit in the top 20
+				context.lineWidth = 0.25 * pixelRatio;
+				context.strokeStyle = 'black';
+				const dotRadius = 2*radius;
+				const dotMargin = 10 * pixelRatio;
+				const xDot = width + dotMargin + dotRadius;
+				const xText = xDot + dotMargin + dotRadius;
 				for (let i = -1; i < cats.length; i++) {
+					let yDot = (i+2) * (2*dotRadius + dotMargin);
 					context.beginPath();
-					context.arc(width + 20, (i + 2) * 15, 7, 0, 2 * Math.PI, false);
+					context.circle(xDot, yDot, dotRadius);
 					context.closePath();
 					// i+1 because white (other) is the first color
 					// and i = 1 would be the first category
 					context.fillStyle = palette[i + 1];
 					context.fill();
-					context.lineWidth = 0.25;
-					context.strokeStyle = 'black';
 					context.stroke();
 					context.fillStyle = 'black';
 					if (i === -1) {
-						context.fillText('(all other categories)', width + 30, (i + 2) * 15 + 3);
+						context.fillText('(all other categories)', xText, yDot + radius);
 					} else {
-						context.fillText(cats[i], width + 30, (i + 2) * 15 + 3);
+						context.fillText(cats[i], xText, yDot + radius);
 					}
 				}
 			} else {
@@ -94,30 +117,34 @@ export class Scatterplot extends React.Component {
 				let original_cmax = Math.max(...color);
 				// Log transform if requested
 				if (this.props.logScaleColor) {
-					color = color.map((x) => { return Math.log2(x + 1); });
+					color = color.map((c) => { return Math.log2(c + 1); });
 				}
 				// Map to the range of colors
 				const cmin = Math.min(...color);
 				const cmax = Math.max(...color);
-				color = color.map((x) => { return palette[Math.round((x - cmin) / (cmax - cmin) * palette.length)]; });
+				color = color.map((c) => { return palette[Math.round((c - cmin) / (cmax - cmin) * palette.length)]; });
 
 				// Draw the color legend
+				context.lineWidth = 0.25 * pixelRatio;
+				context.strokeStyle = 'black';
+				const dotRadius = 2*radius;
+				const dotMargin = 10 * pixelRatio;
+				const xDot = width + dotMargin + dotRadius;
+				const xText = xDot + dotMargin + dotRadius;
 				for (let i = 0; i < palette.length; i++) {
+					let yDot = (i+1) * (2*dotRadius + dotMargin);
 					context.beginPath();
-					context.arc(width + 20, (i + 1) * 15, 7, 0, 2 * Math.PI, false);
+					context.circle(xDot, yDot, dotRadius);
 					context.closePath();
 					// Invert it so max value is on top
 					context.fillStyle = palette[palette.length - i - 1];
 					context.fill();
-					context.lineWidth = 0.25;
-					context.strokeStyle = 'black';
 					context.stroke();
-					context.fillStyle = 'black';
 					if (i === 0) {
-						context.fillText(parseFloat(original_cmax.toPrecision(3)), width + 30, (i + 1) * 15 + 3);
+						context.fillText(parseFloat(original_cmax.toPrecision(3)), xText, yDot + radius);
 					}
 					if (i === palette.length - 1) {
-						context.fillText(parseFloat(original_cmin.toPrecision(3)), width + 30, (i + 1) * 15 + 3);
+						context.fillText(parseFloat(original_cmin.toPrecision(3)), xText, yDot + radius);
 					}
 				}
 			}
@@ -133,11 +160,8 @@ export class Scatterplot extends React.Component {
 				if (color[i] !== current_color) {
 					continue;
 				}
-				const xi = (x[i] - xmin) / (xmax - xmin) * (width - 2 * radius) + radius;
-				// "1-" because Y needs to be flipped
-				const yi = (1 - (y[i] - ymin) / (ymax - ymin)) * (height - 2 * radius) + radius;
-				context.moveTo(xi + radius, yi);
-				context.arc(xi, yi, radius, 0, 2 * Math.PI, false);
+				context.moveTo(x[i] + radius, y[i]);
+				context.circle(x[i], y[i], radius);
 			}
 			context.closePath();
 			context.fillStyle = current_color;
