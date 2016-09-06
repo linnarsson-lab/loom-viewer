b5642408ac55ab43a626fcd3237581f374fbf718
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-09-06 10:51:57 +0200

    Proper resizing of canvas on Firefox

--- client/components/canvas.js
+++ client/components/canvas.js
@@ -1,11 +1,14 @@
 import React, {PropTypes} from 'react';
 import { debounce } from 'lodash';
-import {  ButtonGroup, Button, Glyphicon } from 'react-bootstrap';
 
-
-// A simple helper component, wrapping retina logic for canvas.
-// Expects a "painter" function that takes a "context" to draw on.
-// This will draw on the canvas whenever the component updates.
+// A simple helper component, wrapping retina logic for canvas and
+// auto-resizing the canvas to fill its parent container.
+// To determine size/layout, we just use CSS on the div containing
+// the Canvas component (we're using this with flexbox, for example).
+// Expects a "paint" function that takes a "context" to draw on
+// Whenever this component updates it will call this paint function
+// to draw on the canvas. For convenience, pixel dimensions are stored
+// in context.width, context.height and contex.pixelRatio.
 export class Canvas extends React.Component {
 	constructor(props) {
 		super(props);
@@ -16,18 +19,19 @@ export class Canvas extends React.Component {
 		this.state = {
 			width: 1,
 			height: 1,
+			ratio: window.devicePixelRatio || 1,
 			resizing: true,
 		};
 
 		const resize = () => { this.setState({ resizing: true }); };
+		// Because the resize event can fire very often, we
+		// add a debouncer to minimise pointless
+		// (unmount, resize, remount)-ing of the canvas.
 		this.setResize = debounce(resize, 200);
 	}
 
 	componentDidMount() {
 		this.fitToZoomAndPixelRatio();
-		// Because the resize event can fire very often, we
-		// add a debouncer to minimise pointless
-		// resizing/redrawing of the canvas.
 		window.addEventListener('resize', this.setResize);
 	}
 
@@ -36,35 +40,36 @@ export class Canvas extends React.Component {
 	}
 
 	componentDidUpdate(prevProps, prevState) {
-
-		// If resizing was true, but no longer, this means
-		// that on our last render canvas was unmounted
-		if (prevState.resizing && !this.state.resizing) {
+		// if resizing was not true both before the update,
+		// but is now, then the canvas has been unmounted
+		if (!prevState.resizing && this.state.resizing) {
 			this.fitToZoomAndPixelRatio();
 		}
 
-		// Only call draw if it wasn't already looping;
-		// in the latter case it will re-render on its own
-		if (!prevProps.loop) {
+		if (!this.state.resizing && !prevProps.loop) {
 			this.draw();
 		}
 	}
 
 	// Make sure we get a sharp canvas on Retina displays
 	// as well as adjust the canvas on zoomed browsers
+	// Does NOT scale; painter functions decide how to handle
+	// window.devicePixelRatio on a case-by-case basis
 	fitToZoomAndPixelRatio() {
-		const ratio = window.devicePixelRatio || 1;
 		const view = this.refs.view;
+		const ratio = window.devicePixelRatio || 1;
 		const width = (view.clientWidth * ratio) | 0;
 		const height = (view.clientHeight * ratio) | 0;
-		this.setState({ width, height, ratio });
+		const resizing = false;
+		this.setState({ width, height, ratio, resizing });
 	}
 
-	// Relies on a ref to a DOM element, so only call after mounting!
+	// Relies on a ref to a DOM element, so only call
+	// when canvas element has been rendered!
 	draw() {
-		if (!this.state.resizing) {
-			let canvas = this.refs.canvas;
-			const { width, height, ratio } = this.state;
+		const { width, height, ratio, resizing } = this.state;
+		if (!resizing) {
+			const canvas = this.refs.canvas;
 			let context = canvas.getContext('2d');
 			// store width, height and ratio in context for paint functions
 			context.width = width;
@@ -73,14 +78,19 @@ export class Canvas extends React.Component {
 			// should we clear the canvas every redraw?
 			if (this.props.clear) { context.clearRect(0, 0, canvas.width, canvas.height); }
 			this.props.paint(context);
-			// is the provided paint function an animation?
-			if (this.props.loop) {
-				window.requestAnimationFrame(this.draw);
-			}
+			// is the provided paint function an animation? (not entirely sure about this API)
+		}
+		if (this.props.loop) {
+			window.requestAnimationFrame(this.draw);
 		}
 	}
 
 	render() {
+		// The way canvas interacts with CSS layouting is a bit buggy
+		// and inconsistent across browsers. To make it dependent on
+		// the layout of the parent container, we completely remove
+		// the node when resizing. After calculations are done,
+		// we mount it again at the proper dimensions.
 		let canvas = null;
 		if (!this.state.resizing) {
 			canvas = (<canvas
@@ -92,14 +102,12 @@ export class Canvas extends React.Component {
 					height: '100%',
 				}} />
 			);
-		} else {
-			this.setState({ resizing: false });
 		}
 
 		return (
 			<div
-				className='view'
 				ref='view'
+				className={this.props.className ? this.props.className : 'view'}
 				style={this.props.style}>
 				{canvas}
 			</div>
@@ -111,5 +119,6 @@ Canvas.propTypes = {
 	paint: PropTypes.func.isRequired,
 	clear: PropTypes.bool,
 	loop: PropTypes.bool,
+	className: PropTypes.string,
 	style: PropTypes.object,
 };
