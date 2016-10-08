d1b304db38b05e934263db1c08e8dde964628c5d
Job van der Zwan <j.l.vanderzwan@gmail.com>
2016-10-08 16:44:04 +0200

    Quick test to see if Vega works with plain JS objects
    Answer: it does

--- client/components/violinplot.js
+++ client/components/violinplot.js
@@ -4,55 +4,55 @@ import {createClassFromSpec} from 'react-vega';
 
 function makeViolinPlotSpec(width, height) {
 	return {
-		'width': (width - 60),
-		'height': (height - 60),
-		'padding': { 'top': 10, 'left': 30, 'bottom': 30, 'right': 10 },
-		'data': [{ 'name': 'table' }],
-		'signals': [
+		width: (width - 60),
+		height: (height - 60),
+		padding: { top: 10, left: 30, bottom: 30, right: 10 },
+		data: [{ name: 'table' }],
+		signals: [
 			{
-				'name': 'hover', 'init': null,
-				'streams': [
-					{ 'type': '@bar:mouseover', 'expr': 'datum' },
-					{ 'type': '@bar:mouseout', 'expr': 'null' },
+				name: 'hover', 'init': null,
+				streams: [
+					{ type: '@bar:mouseover', expr: 'datum' },
+					{ type: '@bar:mouseout', expr: 'null' },
 				],
 			},
 		],
-		'scales': [
+		scales: [
 			{
-				'name': 'x',
-				'type': 'ordinal',
-				'range': 'width',
-				'domain': { 'data': 'table', 'field': 'x' },
+				name: 'x',
+				type: 'ordinal',
+				range: 'width',
+				domain: { data: 'table', field: 'x' },
 			},
 			{
-				'name': 'y',
-				'type': 'linear',
-				'range': 'height',
-				'domain': { 'data': 'table', 'field': 'y' },
-				'nice': true,
+				name: 'y',
+				type: 'linear',
+				range: 'height',
+				domain: { data: 'table', field: 'y' },
+				nice: true,
 			},
 		],
-		'axes': [
-			{ 'type': 'x', 'scale': 'x' },
-			{ 'type': 'y', 'scale': 'y' },
+		axes: [
+			{ type: 'x', scale: 'x' },
+			{ type: 'y', scale: 'y' },
 		],
-		'marks': [
+		marks: [
 			{
-				'type': 'rect',
-				'name': 'bar',
-				'from': { 'data': 'table' },
-				'properties': {
-					'enter': {
-						'x': { 'scale': 'x', 'field': 'x' },
-						'width': { 'scale': 'x', 'band': true, 'offset': -1 },
-						'y': { 'scale': 'y', 'field': 'y' },
-						'y2': { 'scale': 'y', 'value': 0 },
+				type: 'rect',
+				name: 'bar',
+				from: { data: 'table' },
+				properties: {
+					enter: {
+						x: { scale: 'x', field: 'x' },
+						width: { scale: 'x', band: true, offset: -1 },
+						y: { scale: 'y', field: 'y' },
+						y2: { scale: 'y', value: 0 },
 					},
-					'update': {
-						'fill': { 'value': 'steelblue' },
+					update: {
+						fill:{ value: 'steelblue' },
 					},
-					'hover': {
-						'fill': { 'value': 'red' },
+					hover: {
+						fill:{ value: 'red' },
 					},
 				},
 			},
