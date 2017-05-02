066d07acdcc00482b5a0657c5344d052728fce55
Sten Linnarsson <sten.linnarsson@ki.se>
2017-05-02 12:11:12 +0200

    Formatting and change repo name

--- python/setup.py
+++ python/setup.py
@@ -5,10 +5,10 @@ __version__ = '0.0.0'
 exec(open('loompy/_version.py').read())
 
 setup(
-    name = "loompy",
-    version = __version__,
-    packages = find_packages(),
-    install_requires = [
+    name="loom-viewer",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
         'matplotlib',
         'scikit-learn',
         'h5py',
@@ -27,13 +27,13 @@ setup(
     scripts=['loompy/loom'],
 
     # static files from MANIFEST.in
-    include_package_data = True,
+    include_package_data=True,
 
     # metadata for upload to PyPI
-    author = "Linnarsson Lab",
-    author_email = "sten.linnarsson@ki.se",
-    description = "Create, browse and manipulate .loom files",
-    license = "BSD",
-    keywords = "loom omics transcriptomics bioinformatics",
-    url = "https://github.com/linnarsson-lab/Loom",
+    author="Linnarsson Lab",
+    author_email="sten.linnarsson@ki.se",
+    description="Create, browse and manipulate .loom files",
+    license="BSD",
+    keywords="loom omics transcriptomics bioinformatics",
+    url="https://github.com/linnarsson-lab/Loom",
 )
