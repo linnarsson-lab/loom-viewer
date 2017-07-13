0e0fb45b8eca8a403d27769404a23f28003da553
JobLeonard <j.l.vanderzwan@gmail.com>
2017-07-13 18:50:25 +0200

    Added mobile detection for RemountOnResize
    On certain mobile devices, the software keyboard
    triggers a resize event. In that case, we do not
    want to trigger the remount. Instead, we want
    to trigger a resize only when switching between
    portrait and landscape modes.

--- client/components/remount-on-resize.js
+++ client/components/remount-on-resize.js
@@ -3,16 +3,42 @@ import PropTypes from 'prop-types';
 
 import { debounce } from 'lodash';
 
+import MobileDetect from 'mobile-detect';
+const isMobile = new MobileDetect(window.navigator.userAgent).mobile();
+
 export class RemountOnResize extends PureComponent {
 	constructor(props) {
 		super(props);
-		this.state = { resizing: true };
-
-		const resize = () => {
-			if (!this.state.resizing) {
-				this.setState({ resizing: true });
-			}
+		this.state = {
+			resizing: true,
+			isPortrait: window.innerHeight > window.innerWidth,
 		};
+
+		// On certain mobile devices, the software keyboard
+		// triggers a resize event. In that case, we do not
+		// want to trigger the remount. Instead, we want
+		// to trigger a resize only when switching between
+		// portrait and landscape modes
+		const resize = isMobile ? (
+			() => {
+				let isPortrait = window.innerHeight > window.innerWidth;
+				if (isPortrait !== this.state.isPortrait || !this.state.resizing) {
+					this.setState({
+						resizing: true,
+						isPortrait,
+					});
+				}
+			}
+		) : (
+			() => {
+				if (!this.state.resizing) {
+					this.setState({
+						resizing: true,
+					});
+				}
+			}
+		);
+
 		// Because the resize event can fire very often, we
 		// add a debouncer to minimise pointless
 		// (unmount, resize, remount)-ing of the child nodes.
@@ -38,6 +64,8 @@ export class RemountOnResize extends PureComponent {
 	}
 
 	componentDidUpdate(prevProps, prevState) {
+		// Yes, this triggers another update.
+		// That is the whole point.
 		if (!prevState.resizing && this.state.resizing) {
 			this.setState({ resizing: false });
 		}
