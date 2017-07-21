810e424a10c19fdd97ed26a91808ebc914c9c667
JobLeonard <j.l.vanderzwan@gmail.com>
2017-07-21 17:18:31 +0200

    NavBar collapse semi-fix
    It still doesn't fully behave as intended, but is less broken than before :/

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,55 +1,62 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import DocumentTitle from 'react-document-title';
 
-export const NavbarView = function (props) {
-	const { project, filename, viewsettings } = props.params;
-	let viewLinks;
-	if (filename) {
-		viewLinks = ['heatmap', 'sparklines', 'cells', 'cellmetadata', 'genes', 'genemetadata'].map(
-			(view) => {
-				const link = `/dataset/${view}/${project}/${filename}/${viewsettings}`;
-				return (
-					<LinkContainer to={link} key={view}>
-						<NavItem eventKey={view}>
-							{view.charAt(0).toUpperCase() + view.slice(1)}
-						</NavItem>
+export class NavbarView extends PureComponent {
+	render() {
+		const { project, filename, viewsettings } = this.props.params;
+		let viewLinks;
+		if (filename) {
+			viewLinks = ['heatmap', 'sparklines', 'cells', 'cellmetadata', 'genes', 'genemetadata'].map(
+				(view) => {
+					const link = `/dataset/${view}/${project}/${filename}/${viewsettings}`;
+					return (
+						<LinkContainer to={link} key={view}>
+							<NavItem eventKey={view}>
+								{view.charAt(0).toUpperCase() + view.slice(1)}
+							</NavItem>
+						</LinkContainer>
+					);
+				}
+			);
+		}
+		const title = project && filename ?
+			`Loom - /${project}/${filename}` : 'Loom';
+
+		const navbarInstance = (
+			<Navbar staticTop collapseOnSelect>
+				<Navbar.Header>
+					<LinkContainer to='/'>
+						<Navbar.Brand>
+							{title}
+						</Navbar.Brand>
 					</LinkContainer>
-				);
-			}
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav>
+						{viewLinks}
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+		return (
+			<DocumentTitle title={title}>
+				<div className='view-vertical'>
+					<div>
+						{navbarInstance}
+					</div>
+					<div className='view'>
+						{this.props.children}
+					</div>
+				</div>
+			</DocumentTitle>
 		);
 	}
-	const title = project && filename ?
-		`Loom - /${project}/${filename}` : 'Loom';
-
-	const navbarInstance = (
-		<Navbar>
-			<Navbar.Header>
-				<LinkContainer to='/'>
-					<Navbar.Brand>
-						{title}
-					</Navbar.Brand>
-				</LinkContainer>
-			</Navbar.Header>
-			<Nav>
-				{viewLinks}
-			</Nav>
-		</Navbar>
-	);
-	return (
-		<DocumentTitle title={title}>
-			<div className='view-vertical'>
-				{navbarInstance}
-				<div className='view'>
-					{props.children}
-				</div>
-			</div>
-		</DocumentTitle>
-	);
-};
+}
 
 NavbarView.propTypes = {
 	// Passed down by react-router
