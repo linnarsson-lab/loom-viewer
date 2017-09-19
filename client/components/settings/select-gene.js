2707c3fa2e7e6ab090ade29ae29a527609176674
JobLeonard <j.l.vanderzwan@gmail.com>
2017-09-19 17:38:53 +0200

    Simplified gene selection in Sparklines

--- client/components/settings/select-gene.js
+++ client/components/settings/select-gene.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Select from 'react-virtualized-select';
 import {
 	FormControl,
-	Button,
 } from 'react-bootstrap';
 
 import {
@@ -13,20 +12,39 @@ import {
 	isEqual,
 } from 'lodash';
 
+function filterValidGenes(selection, geneToRowLowerCase, rowToGenes) {
+	const genes = selection.split(/[,;'"`\s]+/g);
+	let validGenes = [];
+	for (let i = 0; i < genes.length; i++) {
+		const geneIdx = geneToRowLowerCase[genes[i].toLowerCase()];
+		if (geneIdx !== undefined) {
+			validGenes.push(rowToGenes[geneIdx]);
+		}
+	}
+	return uniq(validGenes);
+}
+
 // TODO: document how SelectGeneComponent works and what it expects
 export class SelectGeneComponent extends PureComponent {
 
 	constructor(props) {
 		super(props);
 		this.addSelection = this.addSelection.bind(this);
-		this.applySelection = this.applySelection.bind(this);
 		this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
-		this.filterValidGenes = this.filterValidGenes.bind(this);
 	}
 
 	componentWillMount() {
-		let { selectedGenes, dataset, onChange } = this.props;
-		const { geneKeys } = dataset.col;
+		let {
+			selectedGenes,
+			dataset,
+			onChange,
+		} = this.props;
+
+		const {
+			geneKeys,
+			geneToRowLowerCase,
+			rowToGenes,
+		} = dataset.col;
 
 		let i = geneKeys.length, selectOptions = new Array(i);
 		while (i--) {
@@ -37,101 +55,90 @@ export class SelectGeneComponent extends PureComponent {
 		}
 		let validGenes;
 		if (selectedGenes && selectedGenes.length) {
-			selectedGenes = selectedGenes.join(',\ ');
-			validGenes = this.filterValidGenes(selectedGenes);
+			selectedGenes = selectedGenes.join(', ');
+			validGenes = filterValidGenes(selectedGenes, geneToRowLowerCase, rowToGenes);
 			let diff = difference(selectedGenes, validGenes);
 			let diff2 = difference(validGenes, selectedGenes);
 			if (diff.length + diff2.length) {
 				// difference length is only non-zero when
 				// some values in validGenes have changed
 				onChange(validGenes);
-				selectedGenes = validGenes.join(',\ ');
+				selectedGenes = validGenes.join(', ');
 			}
 		}
 
-		this.setState({ selectOptions, validGenes, selectedGenes });
+		this.setState({
+			selectOptions,
+			validGenes,
+			selectedGenes,
+			geneToRowLowerCase,
+			rowToGenes,
+		});
+	}
+
+	addSelection(selectValue) {
+		const { onChange } = this.props;
+		let {
+			selectedGenes,
+			validGenes,
+			geneToRowLowerCase,
+			rowToGenes,
+		} = this.state;
+		selectedGenes = selectedGenes ? selectedGenes + ' ' + selectValue.value : selectValue.value;
+
+		let newValidGenes = filterValidGenes(selectedGenes, geneToRowLowerCase, rowToGenes);
+
+		if (!isEqual(validGenes, newValidGenes) && onChange) {
+			onChange(newValidGenes);
+		}
+		this.setState({
+			validGenes: newValidGenes,
+			selectedGenes: newValidGenes.join(', '),
+		});
 	}
 
 	handleTextAreaChange(event) {
 		let selectedGenes = event.target.value;
-		let { validGenes } = this.state;
-		switch(event.key){
+		let {
+			validGenes,
+			geneToRowLowerCase,
+			rowToGenes,
+		} = this.state;
+
+		switch (event.key) {
 			case 'Enter':
 			case ' ':
-				validGenes = this.filterValidGenes(selectedGenes);
+			case ',':
+			case ';':
+				validGenes = filterValidGenes(selectedGenes, geneToRowLowerCase, rowToGenes);
 				this.props.onChange(validGenes);
-				selectedGenes = validGenes.join(',\ ');
+				selectedGenes = validGenes.join(', ');
+				break;
 			default:
 		}
-		this.setState({ selectedGenes });
-	}
-
-	addSelection(selectValue) {
-		let { selectedGenes } = this.state;
-		selectedGenes = selectedGenes ? selectedGenes + ', ' + selectValue.value : selectValue.value;
-		this.setState({ selectedGenes });
-	}
-
-	applySelection() {
-		const { onChange } = this.props;
-
-		let validGenes = this.filterValidGenes(this.state.selectedGenes);
-		if (!isEqual(validGenes, this.state.validGenes)) {
-			// We also call onChange if there is no value,
-			// to handle "resetting" the view
-			onChange ? onChange(validGenes) : null;
-
-		}
-		const selectedGenes = validGenes.join(', ');
 		this.setState({ validGenes, selectedGenes });
-	}
-
-	filterValidGenes(selection) {
-		const { col } = this.props.dataset;
-		const genes = selection.split(/[,;'"`\s]+/g);
-
-		let validGenes = [];
-		const { rowToGenes, geneToRowLowerCase } = col;
-		for (let i = 0; i < genes.length; i++) {
-			const geneIdx = geneToRowLowerCase[genes[i].toLowerCase()];
-			if (geneIdx !== undefined) {
-				validGenes.push(rowToGenes[geneIdx]);
-			}
-		}
-		return uniq(validGenes);
 	}
 
 	render() {
 		const col = this.props.dataset.col;
 		return (
-			<div><div className={'view'}>
-				<div style={{ flex: 8 }}>
-					<Select
-						placeholder={'Type to search'}
-						options={this.state.selectOptions}
-						filterOptions={col.dropdownOptions.keyAttr}
-						onChange={this.addSelection}
-						style={this.props.style}
-						maxHeight={100}
-					/>
-				</div>
-				<Button
-					bsStyle={'default'}
-					bsSize='small'
-					style={{ flex: 1 }}
-					onClick={this.applySelection} >
-					Apply
-				</Button>
-        </div>
-			<FormControl
-				className='scroll'
-				componentClass={'textarea'}
-				rows={8}
-				placeholder={'Paste genes here or use the dropdown above to search \n\n(don\'t worry about duplicate or incorrect entries, capitalization, commas, semicolons, or quotations. "Apply Selection" fixes and filters this)'}
-				onChange={this.handleTextAreaChange}
-				onKeyPress={this.handleTextAreaChange}
-				value={this.state.selectedGenes} />
-
+			<div>
+				<Select
+					placeholder={'Type to search'}
+					options={this.state.selectOptions}
+					filterOptions={col.dropdownOptions.keyAttr}
+					onChange={this.addSelection}
+					style={this.props.style}
+					maxHeight={100}
+				/>
+				<FormControl
+					className='scroll'
+					componentClass={'textarea'}
+					rows={8}
+					placeholder={'Paste genes here or use the dropdown above to search \n\n(don\'t worry about capitalization, commas, semicolons, or quotations. This is automatically corrected. Duplicate or incorrect entries are removed)'}
+					onChange={this.handleTextAreaChange}
+					onKeyPress={this.handleTextAreaChange}
+					value={this.state.selectedGenes} />
 			</div>
 		);
 	}
