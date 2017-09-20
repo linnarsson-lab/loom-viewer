2b6777446bc2ba5801f9f163ba1e5c16bb0634c3
JobLeonard <j.l.vanderzwan@gmail.com>
2017-09-20 18:13:14 +0200

    Slimmed down bootstrap a bit
    Removed unused featured (Jumbotron, etc)

--- client/loom.js
+++ client/loom.js
@@ -2,14 +2,20 @@ import { render } from 'react-dom';
 
 // Import all CSS once, at the top level
 
+// react-virtualized-select
 import 'react-select/dist/react-select.css';
 import 'react-virtualized/styles.css';
 import 'react-virtualized-select/styles.css';
+
+// rc-slider
 import 'rc-slider/assets/index.css';
 
 // Modified for crisp zoom
 import './css/leaflet_crisp.css';
+// Required for react-bootstrap
 import './css/bootstrap.css';
+import './css/bootstrap-theme.css';
+// Custom loom CSS
 import './css/loom.css';
 
 import Routes from './components/routes';
