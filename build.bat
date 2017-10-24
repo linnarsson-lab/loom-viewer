4b2ec9761f781db3bf17b74dfcac7c338947e890
Job van der Zwan <j.l.vanderzwan@gmail.com>
2017-10-24 09:48:30 +0200

    minor tweak to build.bat

--- build.bat
+++ build.bat
@@ -22,16 +22,13 @@ IF /i "%1"=="prod" (
     echo ==========================================================
     echo.
     webpack --config=webpack.config.prod.js --progress --profile --colors
-    cd .\python
-    move .\loom_viewer\index.html .\loom_viewer\static\index.html
     echo.
     echo ================================
     echo  Installing loom python package
     echo ================================
     echo.
-    :: I haven't figured out how to both generate correct paths in the webpack HTML
-    :: template, and have the resulting index.html be put in the right folders.
-    :: Yes, this is an ugly hack around my incompetence, but it works - Job
+    cd .\python
+    move .\loom_viewer\index.html .\loom_viewer\static\index.html
     python setup.py install --force
     cd ..
 ) ELSE (
@@ -40,16 +37,13 @@ IF /i "%1"=="prod" (
     echo ================================================================
     echo.
     webpack --config=webpack.config.dev.js --progress --profile --colors
-    cd .\python
-    move .\loom_viewer\index.html .\loom_viewer\static\index.html
     echo.
     echo ================================
     echo  Installing loom python package
     echo ================================
     echo.
-    :: I haven't figured out how to both generate correct paths in the webpack HTML
-    :: template, and have the resulting index.html be put in the right folders.
-    :: Yes, this is an ugly hack around my incompetence, but it works - Job
+    cd .\python
+    move .\loom_viewer\index.html .\loom_viewer\static\index.html
     python setup.py install --force
     cd ..
 )
